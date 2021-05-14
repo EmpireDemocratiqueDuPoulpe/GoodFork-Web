90ab7670d4d8f1613064c2d8a1e1eb93067e9def
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-05-14 17:34:12 +0200

    It is now possible to view menu title, description and ingredients

--- src/pages/MenuDetails/MenuDetails.js
+++ src/pages/MenuDetails/MenuDetails.js
@@ -3,23 +3,44 @@ import PropTypes from "prop-types";
 import MenusDB from "../../global/MenusDB.js";
 import ErrorDisplay from "../../components/ErrorDisplay/ErrorDisplay";
 import LoadingDisplay from "../../components/LoadingDisplay/LoadingDisplay";
+import AdvancedTable, {Header, MixedHeader} from "../../components/AdvancedTable/AdvancedTable.js";
+import UnitsDB from "../../global/UnitsDB";
 
 function MenuDetails(props) {
 	const { match: { params: { menu_id } } } = props;
-	const [ menu, setMenu ] = useState([]);
-	const [ loaded, setLoaded ] = useState(false);
+	const [ menu, setMenu ] = useState();
+	const [ units, setUnits ] = useState();
+	const [ menuLoaded, setMenuLoaded ] = useState(false);
+	const [ unitsLoaded, setUnitsLoaded ] = useState(false);
 	const [ error, setError ] = useState();
 
 	const getMenu = async () => {
 		MenusDB.getById(menu_id)
 			.then(response => {
 				setMenu(response.error ? null : response.menu);
-				setLoaded(true);
+				setMenuLoaded(true);
 				setError(response.error ? response : null);
+
+				if (!response.error) getUnits();
 			})
 			.catch(error => {
 				setMenu(null);
-				setLoaded(true);
+				setMenuLoaded(true);
+				setError(error);
+				console.error(error);
+			});
+	};
+
+	const getUnits = async () => {
+		UnitsDB.getAllAsSelect()
+			.then(response => {
+				setUnits(response.error ? null : response);
+				setUnitsLoaded(true);
+				setError(response.error ? response : null);
+			})
+			.catch(error => {
+				setUnits([]);
+				setUnitsLoaded(true);
 				setError(error);
 				console.error(error);
 			});
@@ -30,15 +51,28 @@ function MenuDetails(props) {
 	return (
 		<React.Fragment>
 			<div className="Page-header">
-				<h3>Menu{loaded ? ` - ${menu.name}` : ""}</h3>
+				<h3>Menu{menuLoaded ? ` - ${menu.name}` : ""}</h3>
 			</div>
 
 			<div className="Page-body">
-				{loaded ? (
+				{menuLoaded && unitsLoaded ? (
 					<React.Fragment>
 						{error ? <ErrorDisplay error={error}/> : (
 							<React.Fragment>
-								<p>Chargé !</p>
+								<p>Description: {menu.description}</p>
+								<h4>Ingrédients: </h4>
+								<AdvancedTable
+									headers={[
+										new Header("ID", { propName: "stock_id", type: "number", required: true, readonly: true, hidden: true }),
+										new Header("Nom", { propName: "name", required: true }),
+										new MixedHeader(
+											new Header("Quantité", { propName: "units", type: "float" }),
+											new Header("Unité", { propName: "units_unit", type: "select", selectOpts: units, hideTitle: true })
+										),
+									]}
+									data={menu.ingredients}
+									autoID={true}
+								/>
 							</React.Fragment>
 						)}
 					</React.Fragment>
