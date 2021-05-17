5cf78180774eb5cf33d3a6a98ff25f1a2ae6d75e
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-05-17 14:28:26 +0200

    Fixed a bug where it was impossible to add a new staff member

--- src/pages/Staff/Staff.js
+++ src/pages/Staff/Staff.js
@@ -13,15 +13,15 @@ export default function Staff() {
 	const [ membersLoaded, setMembersLoaded ] = useState(false);
 	const [ rolesLoaded, setRolesLoaded ] = useState(false);
 	const [ error, setError ] = useState();
-	const [ modalError, setModalError ] = useState();
+	const [ errorModal, setErrorModal ] = useState();
 
 	const addStaff = async member => {
 		UsersDB.addStaff(member)
 			.then(response => {
 				if (!response.error) getStaff();
-				else setModalError(response);
+				else setErrorModal(response);
 			})
-			.catch(setModalError);
+			.catch(setErrorModal);
 	};
 
 	const getStaff = async () => {
@@ -44,18 +44,18 @@ export default function Staff() {
 		UsersDB.update(member)
 			.then(response => {
 				if (!response.error) getStaff();
-				else setModalError(response);
+				else setErrorModal(response);
 			})
-			.catch(setModalError);
+			.catch(setErrorModal);
 	};
 
 	const deleteStaff = async member => {
 		UsersDB.deleteStaff(member)
 			.then(response => {
 				if (!response.error) getStaff();
-				else setModalError(response);
+				else setErrorModal(response);
 			})
-			.catch(setModalError);
+			.catch(setErrorModal);
 	};
 
 	const getRoles = async () => {
@@ -81,22 +81,22 @@ export default function Staff() {
 			</div>
 
 			<div className="Page-body">
-				<ModalError error={modalError}/>
+				<ModalError error={errorModal}/>
 				{membersLoaded && rolesLoaded ? (
 					<React.Fragment>
 						{!error ? (
 							<React.Fragment>
 								<AdvancedTable
 									headers={[
-										new Header("ID", { propName: "user_id", hidden: true, required: true }),
+										new Header("ID", { propName: "user_id", type: "number", required: true, readonly: true, hidden: true }),
 										new Header("Prénom", { propName: "first_name", required: true }),
 										new Header("Nom", { propName: "last_name" }),
 										new Header("E-mail", { propName: "email", type: "email", required: true }),
 										new Header("Rôle", {
-											propName: "role",
+											propName: "role_id",
+											displayPropName: "role",
 											type: "select",
 											selectOpts: roles,
-											defaultValueProp: "role_id",
 											required: true
 										})
 									]}
